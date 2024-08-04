4f39dd73e71fae486679a4a4979a5b0820f4411b
jaspreet <jaspreet@gigaprofits.com>
2024-08-04 19:19:16 -0400

    Initial commit

---
+++ src/main/java/com/question/controllers/QuestionController.java
@@ -0,0 +1,40 @@
+package com.question.controllers;
+
+import com.question.entities.Question;
+import com.question.services.QuestionService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/question")
+public class QuestionController {
+
+    private QuestionService questionService;
+
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    @PostMapping
+    public Question create(@RequestBody Question question){
+        return questionService.create(question);
+    }
+
+    //GetAll
+    @GetMapping
+    public List<Question> getAll(){
+        return  questionService.get();
+    }
+
+    @GetMapping("/{questionId}")
+    public Question getAll(@PathVariable Long questionId){
+        return  questionService.getOne(questionId);
+    }
+    @GetMapping("/quiz/{quizId}")
+    //Get all questions of specific quiz
+    public List<Question> getQuestionsOfQuiz(@PathVariable Long quizId){
+        return questionService.getQuestionsofQuiz(quizId);
+
+    }
+}
